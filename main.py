cbb7d2a84045f63e13bf54efd584613cef8af297
Bob Turlington <zach.fraser88@gmail.com>
2024-05-18 02:35:01 -0300

    Draw Text

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ from lib.ssd1306 import SSD1306_I2C
 # Initialize I2C Bus
 i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=100000)
 
-width, height = 64, 48
+oled_width, oled_height = 64, 48
 device_address = 0x3C
 
 # Initialize OLED
-oled = SSD1306_I2C(width, height, i2c, addr= device_address)
+oled = SSD1306_I2C(oled_width, oled_height, i2c, addr= device_address)
 
-oled.fill(1)
+oled.fill(0)
+oled.show()
+
+oled.text("Hello", 0, 0)
+oled.text("World", 0, 8)
 oled.show()
